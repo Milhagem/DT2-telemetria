fe5dd1b725b13ea88308d9d467611fb900fcc759
thiagoh27 <70301749+thiagoh27@users.noreply.github.com>
2023-03-17 18:27:17 -0300

    adicionei a parte do encoder para teste

--- esp_send_data/esp_send_data.ino
+++ esp_send_data/esp_send_data.ino
@@ -59,6 +59,86 @@ INA_Class INA;
 #define GAIN              3.89 // AMPOP_OUT = LM35 output * GAIN
 
 
+// ########## -------------------- ENCODER DATA -------------------- ##########
+#define MEASURE_PIN 12
+#define WHEEL_CIRCUMFERANCE 1.596/12
+#define SAMPLES 25
+
+
+typedef struct {
+  uint16_t ms;
+  uint16_t revolutions;
+} sample_t;
+
+volatile uint8_t nextSample = 0;
+volatile sample_t samples[SAMPLES] = {};
+
+hw_timer_t * timer = NULL;
+
+// temp vars for computing totals
+volatile sample_t* pSample;
+uint8_t n;
+uint8_t totalRevolutions;
+uint8_t totalSamples;
+uint16_t minMs;
+uint16_t maxMs;
+double rps;
+double rpm;
+double speed;
+double wheel_diameter;
+
+
+// interrupt handler is triggered every 100ms
+void IRAM_ATTR timer_interrupt() {
+  // compute total revolutions and revolutions per second
+  totalRevolutions = 0;
+  totalSamples = 0;
+  minMs = 0;
+  maxMs = 0;
+  rps = 0;
+
+  // iterate thru samples
+  for (int i=0;i<SAMPLES;i++) {
+    n = (nextSample+i) % SAMPLES;
+    pSample = &samples[n];
+
+    if (pSample->ms > 0) {
+      // good sample
+      totalSamples++;
+      totalRevolutions += pSample->revolutions;
+
+      minMs = std::min(minMs, (uint16_t)pSample->ms);
+      maxMs = std::max(maxMs, (uint16_t)pSample->ms);
+    }
+  }
+
+  // compute rps
+  if (totalSamples > 0) {
+    rps = totalRevolutions * 1.0 / totalSamples;
+  }
+
+  // prepare for a new sample
+  if (nextSample >= SAMPLES-1) {
+    nextSample = 0;
+  } else {
+    nextSample++;
+  }
+
+  // clear old sample data
+  samples[nextSample].ms = millis();
+  samples[nextSample].revolutions = 0;
+}
+
+// interrupt handler is triggered when magnet enters or leaves field
+void magnet_detection_changed() {
+  // check if magnet is nearby
+  if (digitalRead(MEASURE_PIN) == HIGH) {
+    // increase revolutions
+    samples[nextSample].revolutions++;
+  }
+}
+
+
 //-----------------------------------Replace with your network credentials----------------------------------------
 // Replace with your network credentials
 const char* ssid     = "";
@@ -69,6 +149,10 @@ const char* serverName = "";
 
 // Keep this API Key value to be compatible with the PHP code provided in the server.
 String apiKeyValue = "";
+
+
+
+
 //----------------------------------------------------------------------------------------------------------------
 
 void setup() {
@@ -116,6 +200,30 @@ void setup() {
   SerialMon.println("Modem Info: " + modemInfo);
 
   Serial.println("All done with GPS! Starting HTTP config...");
+  
+  // ------------------- ENCODER SETUP --------------------
+  
+  
+  Serial.begin(115200);
+  
+  pinMode(MEASURE_PIN, INPUT);
+
+  // prescaler 80 is for 80MHz clock. One tick of the timer is 1us
+  timer = timerBegin(0, 80, true); 
+
+  // attach interrupt handler
+  timerAttachInterrupt(timer, &timer_interrupt, true); 
+
+  // trigger timer every 100ms (100,000us)
+  timerAlarmWrite(timer, 100000, true); 
+
+  // enable the timer
+  timerAlarmEnable(timer); 
+
+  // trigger function when hall effect sensor changes (goes low->high or high->low)
+  attachInterrupt(MEASURE_PIN, magnet_detection_changed, CHANGE);
+  
+  
 
   // ------------------- WIFI SETUP --------------------
   WiFi.begin(ssid, password);
@@ -129,6 +237,13 @@ void setup() {
   Serial.println(WiFi.localIP());
 
 }
+
+
+
+
+
+
+
 
 
 void loop() {
@@ -259,6 +374,19 @@ void loop() {
   milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
   celcius = milliVolt / 10;
   farenheits = celcius * 9 / 5 + 32;
+  
+  // -------------------------------------------------------------------------------------------------
+
+
+  // |_________________________________________________|
+  // |___________________  ENCODER   __________________|
+  // |_________________________________________________|
+  
+  rpm = rps * 60 * 0.333;
+  speed = rps * 60 * 0.333 * WHEEL_CIRCUMFERANCE;
+  wheel_diameter = WHEEL_CIRCUMFERANCE;
+  
+
 
   // -------------------------------------------------------------------------------------------------
 
@@ -282,7 +410,8 @@ void loop() {
                               "&celcius=" + String(celcius) + "&farenheits=" + String(farenheits) + 
                               "&voltage_battery=" + String(voltage_battery, 1) + 
                               "&current_motor=" + String(current_motor, 1) + "&power=" + String(power, 1) + 
-                              "&consumption=" + String(consumption, 1) + 
+                              "&consumption=" + String(consumption, 1) + "&rpm=" + String(rpm, 1) + 
+                              "&speed=" + String(speed, 1) +  "&consumption=" + String(wheel_diameter, 1) +                     
                               "&reading_time=" + reading_time + "";
 
     Serial.print("httpRequestData: ");
@@ -316,3 +445,6 @@ void loop() {
     Serial.println(WiFi.localIP());;
   }
 }
+
+
+
