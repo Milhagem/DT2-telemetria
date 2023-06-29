fea18a7b3ba6ed79dba83d11aab9e344108048ed
camposouza <guilhermesouza0911@outlook.com>
2023-06-29 14:12:35 -0300

    novos comentarios

--- t-sim7000g/gps.hpp
+++ t-sim7000g/gps.hpp
@@ -37,7 +37,13 @@ private:
     int   sec;
 
 public:
+    /**
+     * @brief Esta funcao deve ser chamada no setup() do t-sim7000g para iniciar o GPS
+    */
     void setupGPS();
+    /**
+     * @brief Atualiza todos os atributos do GPS
+     */
     void atualizaGPS();
     /**
      * @brief Imprime todos os atributos do GPS (lat, lon, speed, alt...) na saida Serial
