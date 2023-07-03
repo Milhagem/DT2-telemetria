5bec8546e8c4703f29cbaf5ad2d5f220dae0bbb1
camposouza <guilhermesouza0911@outlook.com>
2023-07-03 16:52:33 -0300

    correcao pino

--- t-sim7000g/lm35.cpp
+++ t-sim7000g/lm35.cpp
@@ -1,12 +1,12 @@
 #include "lm35.hpp"
 
-float LM35::atualizaLM35(){
+void LM35::atualizaLM35(){
     // Le o valor de na saida do AmpOp e divide pelo ganho
     int adcValAmpOp = analogRead(AMPOP_OUT);
-    int adcVal = adcValAmpOp / GAIN;
+    float  adcVal = adcValAmpOp / GAIN;
     // Converte o valor analogico p/ milivolt
-    float milliVolt = adcVal * (ADC_VREF_MV/ ADC_RESOLUTION);
-    // Convete a temsao para temperatura em ºC
+    float milliVolt = adcVal * (ADC_VREF_MV / ADC_RESOLUTION);
+    // Converte a tensao para temperatura em °C
     this->temperatura = milliVolt / LM35_SCALE;
 }
 
--- t-sim7000g/lm35.hpp
+++ t-sim7000g/lm35.hpp
@@ -5,9 +5,9 @@
 
 #define ADC_VREF_MV    3300.0
 #define ADC_RESOLUTION 4096
-#define AMPOP_OUT      35  // conferir esse pino
+#define AMPOP_OUT      32  // conferir esse pino
 #define GAIN           3.89
-#define LM35_SCALE     10
+#define LM35_SCALE     10  // 10 mV / 1 °C
 
 class LM35 {
 private:
@@ -17,7 +17,7 @@ public:
     /**
      * @brief Atualiza o valor de temperatura do sensor LM35
     */
-    float atualizaLM35();
+    void atualizaLM35();
     /**
      * @brief Imprime a temperatura na saida Serial;
     */
