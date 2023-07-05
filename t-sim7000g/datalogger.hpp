ac0c16c1d6305dc0fcbef142103a550e5c45404e
camposouza <guilhermesouza0911@outlook.com>
2023-07-05 12:24:09 -0300

    comentario concateanArquivo()

--- t-sim7000g/datalogger.hpp
+++ t-sim7000g/datalogger.hpp
@@ -22,12 +22,18 @@ public:
      * @brief Esta funcao deve ser chamada no setup() do t-sim7000g para iniciar o Datalogger
     */
     void dataloggerSetup();
-        /**
+    /**
      * @brief Abre o arquivo passado no path se ele existir. Se nao, cria o arquivo e adiciona o cabecalho
      * 
      * @param path caminho para o arquivo. Ex: /nome_arquivo.txt
-     */
+    */
     void abreArquivo(String path);
+    /**
+     * @brief Adiciona mensagem ao final do arquivo
+     * 
+     * @param path caminho para o arquivo. Ex: /nome_arquivo.txt
+     * @param message mensagem a ser adicionada
+    */
     void concatenaArquivo(String path, String message);
     String lePrimeiraLinha(File file);
     String leUltimaLinha(File file);
