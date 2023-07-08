258c6ff716315eb1d4d7b847b28913350ffff9f8
camposouza <guilhermesouza0911@outlook.com>
2023-07-08 14:47:56 -0300

    Codigo para teste encoder (funcionando!)

--- teste_componentes/encoder/encoder.ino
+++ teste_componentes/encoder/encoder.ino
@@ -1,118 +1,39 @@
-/* https://gist.github.com/joshnuss/52a2dae98c4037ef70954432ee798fda
- * Using digital hall effect sensor SENS-M-10 (purchased at Abra)
- * 
- * MCU Board: ESP2-WROOM-32
- */
+#define ENCODER_PIN 25
 
-#define MEASURE_PIN 2
-#define WHEEL_CIRCUMFERANCE 1.596/12
-#define SAMPLES 25
+int rpm;
+volatile byte pulsos;
+unsigned long timeold;
 
+unsigned int pulsos_por_volta = 16;
 
-typedef struct {
-  uint16_t ms;
-  uint16_t revolutions;
-} sample_t;
-
-volatile uint8_t nextSample = 0;
-volatile sample_t samples[SAMPLES] = {};
-
-hw_timer_t * timer = NULL;
-
-// temp vars for computing totals
-volatile sample_t* pSample;
-uint8_t n;
-uint8_t totalRevolutions;
-uint8_t totalSamples;
-uint16_t minMs;
-uint16_t maxMs;
-double rps;
-
-// interrupt handler is triggered every 100ms
-void IRAM_ATTR timer_interrupt() {
-  // compute total revolutions and revolutions per second
-  totalRevolutions = 0;
-  totalSamples = 0;
-  minMs = 0;
-  maxMs = 0;
-  rps = 0;
-
-  // iterate thru samples
-  for (int i=0;i<SAMPLES;i++) {
-    n = (nextSample+i) % SAMPLES;
-    pSample = &samples[n];
-
-    if (pSample->ms > 0) {
-      // good sample
-      totalSamples++;
-      totalRevolutions += pSample->revolutions;
-
-      minMs = std::min(minMs, (uint16_t)pSample->ms);
-      maxMs = std::max(maxMs, (uint16_t)pSample->ms);
-    }
-  }
-
-  // compute rps
-  if (totalSamples > 0) {
-    rps = totalRevolutions * 1.0 / totalSamples;
-  }
-
-  // prepare for a new sample
-  if (nextSample >= SAMPLES-1) {
-    nextSample = 0;
-  } else {
-    nextSample++;
-  }
-
-  // clear old sample data
-  samples[nextSample].ms = millis();
-  samples[nextSample].revolutions = 0;
+void contador() {
+  pulsos++;
 }
-
-// interrupt handler is triggered when magnet enters or leaves field
-void magnet_detection_changed() {
-  // check if magnet is nearby
-  if (digitalRead(MEASURE_PIN) == HIGH) {
-    // increase revolutions
-    samples[nextSample].revolutions++;
-  }
-}
-
 
 void setup() {
   Serial.begin(115200);
+  //Pino do sensor como entrada
+  pinMode(ENCODER_PIN, INPUT);
   
-  pinMode(MEASURE_PIN, INPUT);
-
-  // prescaler 80 is for 80MHz clock. One tick of the timer is 1us
-  timer = timerBegin(0, 80, true); 
-
-  // attach interrupt handler
-  timerAttachInterrupt(timer, &timer_interrupt, true); 
-
-  // trigger timer every 100ms (100,000us)
-  timerAlarmWrite(timer, 100000, true); 
-
-  // enable the timer
-  timerAlarmEnable(timer); 
-
-  // trigger function when hall effect sensor changes (goes low->high or high->low)
-  attachInterrupt(MEASURE_PIN, magnet_detection_changed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), contador, FALLING);
+  pulsos = 0;
+  rpm = 0;
+  timeold = 0;
 }
 
 void loop() {
-  // output calculations for debug
-  Serial.print("samples=");
-  Serial.print(totalSamples);
-  Serial.print(", revolutions=");
-  Serial.print(totalRevolutions);
-  Serial.print(", rps=");
-  Serial.print(rps);
-  Serial.print(", rpm=");
-  Serial.print(rps * 60 * 0.333);
-  Serial.print(", speed=");
-  Serial.print(rps * 60 * 0.333 * WHEEL_CIRCUMFERANCE);  
-  Serial.println();
+  //Atualiza contador a cada segundo
+  if (millis() - timeold >= 1000) {
+    //Desabilita interrupcao durante o calculo
+    detachInterrupt(0);
+    rpm = (60 * 1000 / pulsos_por_volta ) / (millis() - timeold) * pulsos;
+    timeold = millis();
+    pulsos = 0;
 
-  delay(1000);
+    Serial.print("RPM = ");
+    Serial.println(rpm, DEC);
+    
+    //Habilita interrupcao
+    attachInterrupt(0, contador, FALLING);
+  }
 }
