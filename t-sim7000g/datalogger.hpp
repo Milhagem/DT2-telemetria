18c2d9930490f219911ab08d78ff3ed9c244ae19
camposouza <guilhermesouza0911@outlook.com>
2023-07-15 22:20:25 -0300

    Comecando documentacao

--- t-sim7000g/datalogger.hpp
+++ t-sim7000g/datalogger.hpp
@@ -1,6 +1,20 @@
 #ifndef DATALOGGER_H
 #define DATALOGGER_H
 
+/**
+ * @file datalogger.hpp
+ * @author camposouza(guilhermesouza0911@ufmg.br)
+ * @brief  Datalogger Class header file
+ * @version 0.1
+ * @date 2023-07-15
+ * 
+ * @details Classe responsavel por criar um arquivo.txt e gravar nele todos os dados lidos pelos sensores.
+ * Esta classe possui um metodo setupDatalogger() que deve ser chamada na funcao main.ino para funcionar corretamente,
+ * alem de um metodo abreArquivo() que tambem deve ser chamado no setup(). Feito isso,
+ * Carregue o codigo na placa t-sim7000g sem o cartao SD. Em seguida, coloque o cartao SD e aperte o botao RST (reset).
+ * Nesse ponto, o cartao deve ser montado com sucesso.
+ */
+
 #include "FS.h"
 #include "SD_MMC.h"
 
---
+++ teste_componentes/README.md
@@ -0,0 +1,7 @@
+## Código para teste dos modulos
+
+Essa pasta contém códigos validados de fácil acesso para testar individualmente as features da placa da telemetria e da placa T-sim700g.
+
+Use-os para saber se um sensor não está queimado, ou para testar a placa da telemetria, por exemplo.
+
+**Os código contidos aqui não necessariamente são iguais ao t-sim 7000g.ino**
