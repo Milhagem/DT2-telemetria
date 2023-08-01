bd1a678dc9a0dfd59cc3b61e2bb9c36b4e6ace64
camposouza <guilhermesouza0911@outlook.com>
2023-08-01 15:07:33 -0300

    add lte_communication (funcional)

---
+++ RTOS/lte_communication.cpp
@@ -0,0 +1,148 @@
+#include "lte_communication.hpp"
+
+TinyGsm modem(SerialAT);
+
+void LTE_Communication::modemPowerOn(){
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, LOW);
+    delay(1000);
+    digitalWrite(PWR_PIN, HIGH);
+}
+
+void LTE_Communication::modemPowerOff(){
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, LOW);
+    delay(1500);
+    digitalWrite(PWR_PIN, HIGH);
+}
+
+void LTE_Communication::modemRestart(){
+    modemPowerOff();
+    delay(1000);
+    modemPowerOn();
+}
+
+
+void LTE_Communication::setupLTE() {
+  // Set console baud rate
+  SerialMon.begin(115200);
+
+  delay(10);
+
+  // Set LED OFF
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  modemPowerOn();
+
+  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+
+  Serial.println("/**********************************************************/");
+  Serial.println("To initialize the network test, please make sure your LTE ");
+  Serial.println("antenna has been connected to the SIM interface on the board.");
+  Serial.println("/**********************************************************/\n\n");
+
+  delay(10000);
+
+  String res;
+
+  Serial.println("========INIT========");
+
+  if (!modem.init()) {
+    modemRestart();
+    delay(2000);
+    Serial.println("Failed to restart modem, attempting to continue without restarting");
+    return;
+  }
+
+  Serial.println("========SIMCOMATI======");
+  modem.sendAT("+SIMCOMATI");
+  modem.waitResponse(1000L, res);
+  res.replace(GSM_NL "OK" GSM_NL, "");
+  Serial.println(res);
+  res = "";
+  Serial.println("=======================");
+
+  Serial.println("=====Preferred mode selection=====");
+  modem.sendAT("+CNMP?");
+  if (modem.waitResponse(1000L, res) == 1) {
+    res.replace(GSM_NL "OK" GSM_NL, "");
+    Serial.println(res);
+  }
+  res = "";
+  Serial.println("=======================");
+
+
+  Serial.println("=====Preferred selection between CAT-M and NB-IoT=====");
+  modem.sendAT("+CMNB?");
+  if (modem.waitResponse(1000L, res) == 1) {
+    res.replace(GSM_NL "OK" GSM_NL, "");
+    Serial.println(res);
+  }
+  res = "";
+  Serial.println("=======================");
+
+
+  String name = modem.getModemName();
+  Serial.println("Modem Name: " + name);
+
+  String modemInfo = modem.getModemInfo();
+  Serial.println("Modem Info: " + modemInfo);
+
+  // Unlock your SIM card with a PIN if needed
+  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(GSM_PIN);
+  }
+
+  for (int i = 0; i <= 4; i++) {
+    uint8_t network[] = {
+        2,  /*Automatic*/
+        13, /*GSM only*/
+        38, /*LTE only*/
+        51  /*GSM and LTE only*/
+    };
+    Serial.printf("Try %d method\n", network[i]);
+    modem.setNetworkMode(network[i]);
+    delay(3000);
+    bool isConnected = false;
+    int tryCount = 60;
+    while (tryCount--) {
+      int16_t signal =  modem.getSignalQuality();
+      Serial.print("Signal: ");
+      Serial.print(signal);
+      Serial.print(" ");
+      Serial.print("isNetworkConnected: ");
+      isConnected = modem.isNetworkConnected();
+      Serial.println( isConnected ? "CONNECT" : "NO CONNECT");
+      if (isConnected) {
+        break;
+      }
+      delay(1000);
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    }
+    if (isConnected) {
+        break;
+    }
+  }
+  digitalWrite(LED_PIN, HIGH);
+
+  Serial.println();
+  Serial.println("Device is connected .");
+  Serial.println();
+
+
+  Serial.println("/**********************************************************/");
+  Serial.println("After the network test is complete, please enter the  ");
+  Serial.println("AT command in the serial terminal.");
+  Serial.println("/**********************************************************/\n\n");
+
+  while (1) {
+    while (SerialAT.available()) {
+      SerialMon.write(SerialAT.read());
+    }
+    while (SerialMon.available()) {
+      SerialAT.write(SerialMon.read());
+    }
+  }
+}
