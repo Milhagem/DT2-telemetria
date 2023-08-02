cbacb99c950357a724c7c434c616687077c984ed
brrbelizario <brenda0belizario@gmail.com>
2023-08-02 10:30:54 -0300

    codigo documentado encoder.cpp atualizado

--- RTOS/encoder.cpp
+++ RTOS/encoder.cpp
@@ -1,119 +1,91 @@
 #include "encoder.hpp"
 
-
-volatile byte pulsos;
-void contador() {
-  pulsos++;
+volatile byte pulses;
+void counter() {
+    pulses++;
 }
 
+// Method to set up the encoder
 void Encoder::setupEncoder() {
-  attachInterrupt(digitalPinToInterrupt(MEASURE_PIN), contador, FALLING);
+    // Configure the interrupt to detect pulse count
+    attachInterrupt(digitalPinToInterrupt(MEASURE_PIN), counter, FALLING);
 
-  double speed            = 0;
-  double average_speed    = 0;
-  double distancia_total  = 0;
+    // Variables for speed and distance calculations
+    double speed = 0;
+    double average_speed = 0;
+    double total_distance = 0;
 
-  this->tempo_inicio      = 0;
-  this->tempo_speed_old   = 0;
-  this->tempo_speed_atual = 0;
-  this->tempo_delta       = 0;
-  this->tempo_total       = 0;
-  this->speed_old         = 0;
-  this->distancia_trecho  = 0;
-  this->ja_andou          = false;
+    // Initialize control variables
+    this->start_time = 0;
+    this->last_speed_time = 0;
+    this->current_speed_time = 0;
+    this->time_delta = 0;
+    this->total_time = 0;
+    this->previous_speed = 0;
+    this->segment_distance = 0;
+    this->has_started = false;
 }
-
 
 volatile unsigned long last_pulse_time = 0;
-double Encoder::amostraVoltasNew() {
-  int rpm = 0;
 
-  if (this->speed >= MINIMAL_SPEED) {
-    
-    // Atualiza contador a cada segundo
-//  if (millis() - timeold >= 1000) {
-    
-    // Desabilita interrupcao durante o calculo
-//    detachInterrupt(0);
-//    rpm = (60 * 1000 / SAMPLES ) / (millis() - timeold) * pulsos; 
+// Method to sample the encoder's revolutions and calculate RPM
+double Encoder::sampleRevolutions() {
+    int rpm = 0;
 
-//    timeold = millis();
-//    pulsos = 0;
-
-//    Serial.print("RPM = ");
-//    Serial.println(rpm, DEC);
-    
-    // Habilita interrupcao
-//    attachInterrupt(0, contador, FALLING);
-//  }
-
-      // Calcula o intervalo de tempo desde o último pulso
-      unsigned long intervalo_tempo = millis() - last_pulse_time;
-
-      // Atualiza a RPM somente se houver pelo 16 pulsos recebido
-      if (pulsos > SAMPLES) {
-
+    if (this->speed >= MINIMAL_SPEED) {
+        // testar Atualiza contador a cada segundo
+        //  if (millis() - timeold >= 1000) {
+            
         // Desabilita interrupcao durante o calculo
-        detachInterrupt(0);
-        rpm = (60 * 1000 / SAMPLES) / intervalo_tempo * pulsos;
+        //    detachInterrupt(0);
+        //    rpm = (60 * 1000 / SAMPLES ) / (millis() - timeold) * pulsos; 
 
-        pulsos = 0; // Zera o contador de pulsos após calcular a RPM
+        //    timeold = millis();
+        //    pulsos = 0;
 
-        Serial.print("RPM = ");
-        Serial.println(rpm, DEC);
-
-        attachInterrupt(0, contador, FALLING);
-}
-}
-  return rpm;
-}
-
-
-//unsigned long timeold = 0;
-//double Encoder::amostraVoltas() {
-//  int rpm = 0;
-
-  
-//  return rpm;
-//}
+        //    Serial.print("RPM = ");
+        //    Serial.println(rpm, DEC);
+            
+            // Habilita interrupcao
+        //    attachInterrupt(0, contador, FALLING);
+        //  }
+      
 
 
-void Encoder::calculaVelocidade(double rpm) {
-  distancia_trecho = 0;  // metros
-  
-  double speed_mpm = rpm * WHEEL_CIRCUMFERANCE; // metros por minuto
-  this->speed = (speed_mpm * (60.0/1000.0));        // km/h
+            // Calculate the time interval since the last pulse
+            unsigned long time_interval = millis() - last_pulse_time;
 
-  if(speed != 0 && this->ja_andou == false) {  // Começou a andar;
-    this->tempo_inicio = millis();
-    this->tempo_speed_old = millis();
-    this->tempo_speed_atual = millis();
-    this->speed_old = speed_mpm;
-    this->ja_andou = true;
+            // Update RPM only if at least 16 pulses have been received
+            if (pulses > SAMPLES) {
+                // Disable the interrupt during the calculation
+                detachInterrupt(0);
+                rpm = (60 * 1000 / SAMPLES) / time_interval * pulses;
 
-  } else if (this->ja_andou == true){
-    this->tempo_speed_atual = millis();
-    this->tempo_total = this->tempo_speed_atual - this->tempo_inicio;
-    this->tempo_delta = this->tempo_speed_atual - this->tempo_speed_old;
-    this->distancia_trecho = speed_mpm * (this->tempo_delta * ms_TO_min);
-    this->distancia_total = this->distancia_total + this->distancia_trecho/1000.0;
-    this->average_speed = this->distancia_total / ((this->tempo_total * ms_TO_min)/60.0);
-    this->speed_old = speed_mpm;
-    this->tempo_speed_old = this->tempo_speed_atual;
-  }
+                pulses = 0; // Reset the pulse counter after calculating RPM
+
+                Serial.print("RPM = ");
+                Serial.println(rpm, DEC);
+
+                attachInterrupt(0, counter, FALLING);
+            }
+    }
+    return rpm;
 }
 
+// Method to calculate the encoder's speed in km/h
+void Encoder::calculateSpeed(double rpm) {
+    segment_distance = 0;  // meters
 
-void Encoder::imprimir() {
-  Serial.print("Velocidade: ");
-  Serial.print(this->speed);
-  Serial.println(" km/h ");
-  Serial.print("Velocidade media :");
-  Serial.print(this->average_speed);
-  Serial.println(" km/h");
-  Serial.print("Distancia total :");
-  Serial.print(this->distancia_total);
-  Serial.println(" km");
-  Serial.print("Tempo total :");
-  Serial.println(this->tempo_total/1000.0);
-}
+    double speed_mpm = rpm * WHEEL_CIRCUMFERENCE; // meters per minute
+    this->speed = (speed_mpm * (60.0 / 1000.0)); // km/h
+
+    if (speed != 0 && this->has_started == false) {  // Started moving;
+        this->start_time = millis();
+        this->last_speed_time = millis();
+        this->current_speed_time = millis();
+        this->previous_speed = speed_mpm;
+        this->has_started = true;
+    } else if (this->has_started == true) {
+        this->current_speed_time = millis();
+        this->total_time = this->current_speed_time - this->start_time;
+        this->time_delta = this->current_speed_time - this->last_speed_time;
