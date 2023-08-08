8c296309e7d2e8fad7bf21858f4c5f9ad1476b18
felipefacury <felipe-facury@hotmail.com>
2023-08-07 23:13:01 -0300

    Retirada de código desnecessário

--- RTOS/src/lte_connection.cpp
+++ RTOS/src/lte_connection.cpp
@@ -125,20 +125,6 @@ void LTE_Connection::setupLTE() {
   Serial.println("Device is connected .");
   Serial.println();
 
-
-  Serial.println("/**********************************************************/");
-  Serial.println("After the network test is complete, please enter the  ");
-  Serial.println("AT command in the serial terminal.");
-  Serial.println("/**********************************************************/\n\n");
-
-  while (1) {
-    while (SerialAT.available()) {
-      SerialMon.write(SerialAT.read());
-    }
-    while (SerialMon.available()) {
-      SerialAT.write(SerialMon.read());
-    }
-  }
 }
 
 // ---------------------------------------------------------------------------------------------------
