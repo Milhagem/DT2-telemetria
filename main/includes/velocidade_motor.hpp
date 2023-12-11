28096659f2a2b801ba777c8eb2b14cca63399d2e
ThiagoWriel <thiago.wriel.s@gmail.com>
2023-12-11 15:00:04 -0300

    Documentando codigo da velocidade do motor

--- main/includes/velocidade_motor.hpp
+++ main/includes/velocidade_motor.hpp
@@ -13,9 +13,26 @@
 #include <math.h>
 #include <Arduino.h>
 #define hall_motor 34                    // Define o pino que saira as informacoes do hall
-#define circunferencia_do_motor 0.1634  // Circunferencia do motor em m
+#define circunferencia_do_motor 0.05135  // Circunferencia do motor em m
 #define imas_motor 4                   // Define a quantidades de imas que o motor tem
-/// @brief
+
+/**
+ * @class A classe a baixo é responsável por calcular a velocidade do motor com base na variacao
+ * dos sinais do hall do motor e no calculo do intervalo de tempo desses sinais
+ * Essa classe nao segue o mesmo padrão das outras, como a do Encoder, pois a função que ambas 
+ * utilizam para perceber a variação de sinais, AttachInterrupt é tratada de forma diferente, 
+ * onde, nesse código,a função só funciona se estiver associada a funções do escorpo/globais
+ * Em vista disso, esse arquivo .hpp acabou sendo responsável apenas por gerar a impressao dos 
+ * dados e inicializar o calculo
+ * Caso exista alguma outra estratégia de código para contornar isso, o autor do código está aberto
+ * a ajudar e a fazer as devidas alteracoes
+*/
+
+/**
+ * @details Antes desse código ser commitado, ele foi devidamente compilado e executado, funcionando
+ * normalmente, porém apresentando os problemas provindos do atraso do attach e da ineficiencia da
+ * média móvel
+*/
 class Velocidade_Motor {
 private:
 
--- main/src/velocidade_motor.cpp
+++ main/src/velocidade_motor.cpp
@@ -1,83 +1,123 @@
 #include "velocidade_motor.hpp"
 
-Velocidade_Motor* Velocidade_Motor::instance = nullptr;
+/**
+ * @note Esse é o arquivo .cpp vinculado à classe velocidade_motor
+ * Praticamente todos os métodos responsáveis pelo calculo da velocidade estão localizados aqui
+ * em vista o motivo já declarado no arquivo .cpp
+*/
 
-void Velocidade_Motor :: calculo_velocidade (){
-    tempo_atual = micros();
-    if (conta_motor == HIGH){
-        // Utilizando a formula 3.6*(Circunferencia/Periodo), podemos encontrar a velocidade instantanea em Km/h
-        velocidade_instantanea_motor = 3.6*(circunferencia_do_motor/diferenca_pulsos_motor*10e-6*imas_motor);
-        
-        // Atualizar o tempo anterior para o atual para termos uma maior precisao no proximo calculo
-        tempo_anterior_motor = tempo_atual;
+#define circunferencia_do_motor 0.05135  // Circunferencia do motor em m
 
-        // Mudar o estado do conta_motor e o contagem_motor para 0 
-        // para que o calculo_intervalo_hall seja feito corretamente
-        conta_motor = LOW;
-        contagem_motor = 0;
-    }
+volatile bool pulsos;         // Serve para contar se houve uma sequencia efetiva de, por exemplo, 1 0 1
+bool conta = false;           // Controla quando o calculo da velocidade eh realizado
+unsigned long tempNow = 0;    // Tempo em microsegundos medido no operador da velocidade
+unsigned long temp1 = 0;      // (Momento anterior ao pulso anterior)
+unsigned long temp2 = 0;      // Atualiza toda vez que um pulso eh detectado (momento anterior ao pulso atual)
+unsigned long deltaT = 0;     // Delta t
+float velocidade = 0;         // Armazena o valor de velocidade em km/h
+
+
+/**
+ * @brief As variáveis a baixo sao responsaveis pelo calculo da média móvel
+ * Esse tipo de filtro dos valores obtidos da velocidade foi necessário, pois, infelizmente, a funcao 
+ * que detecta a variacao dos pulsos nao funciona direito com variacoes quase que instantaneas e alguns
+ * valores saem com uma variacao muito discrepante de outros 
+*/
+/**
+ * Essa variavel armazena a quantidade de resultados da velocidade que soferao uma media 
+ * Esse valor pode ser alterado caso necessario, mas se um valor muito grande for colocado,
+ * o valor da velocidade perdera precisao em valor e em tempo, uma vez que os dados demorarao
+ * de ser informados. Esse método ainda não é o mais eficiente, uma vez que os valores quando
+ * passam pela media estao tendo uma variacao relativamente significativa por causa dos valores
+ * fora da realidade causados pelo delay do attachinterrupt
+ * Caso exista algum outro método de filtar o código e acabar com os valores fora da realidade
+ * o código pode ser alterado facilmente mexendo na funcao imprimir e adicionando novos metodos
+*/
+const int tamanhoMediaMovel = 14;
+
+// Armazena os valores da velocidade
+float ultimosValores[tamanhoMediaMovel];
+
+int indiceAtual = 0;
+
+
+/**
+ * @brief Essa funcao é responsavel por fazer a troca dinamica das variaveis que armazenam os valores
+ * de tempo para o calculo do delta t
+ * Ela tambem depende diretamente da funcao que calcula a velocidade
+ * Ela está atrelada ao AttachInterrupt, ou seja, toda vez que uma variacao do sinal é percebida pelos
+ * sensores hall do motor, essa funcao é chamada
+ * E, por causa dela, a maioria dos métodos nao foram declarados no .hpp, pois essa funcao precisava ser
+ * global
+*/
+void contador() {
+   pulsos++;
+   if (conta){
+      temp1 = micros ();
+      deltaT = temp1 - temp2;
+      conta = false;
+   } else {
+      temp2 = micros();
+   }
 }
 
-void Velocidade_Motor::hallInterruptHandler() {
-    if (instance) {
-        instance->calculo_intervalo_hall();
-    }
-}
 
-void Velocidade_Motor :: calculo_intervalo_hall (){
-    // Se o codigo entrou aqui, eh porque o estado do hall mudou, entao aumentamos em 1 a contagem do motor
-    contagem_motor++;
-
-    // Se o hall teve uma sequencia, de por exemplo 1 0 1, ele entra na funcao para permitir o calculo da velocidade
-    if (conta_motor % 2 == 0){
-        
-        // Coloca o tempo atual na variavel para que o delta t seja calculado
-        tempo_anterior1_motor = micros();
-
-        // Calculo do delta t ou do 1/4 do periodo 
-        // Utiliza o tempo anterior que foi o tempo atual quando o if do conta motor nao foi satisfeito 
-        diferenca_pulsos_motor = tempo_anterior1_motor - tempo_anterior_motor;
-
-        // Permite que o calculo da velocidade seja feito 
-        conta_motor = HIGH;
-
+/**
+ * @brief Essa funcao recebe o valor do delta t e com base em calculos fisicos, lanca o valor
+ * da velocidade e habilita os if da funcao contador()
+ * Um problema a ser relatado a respeito do calculo é em relacao ao deltaT; os autores originais
+ * do codigo da velocidade do motor nao especificaram direto que periodo seria o do deltaT, de uma
+ * volta completa ou parcial, por exemplo; Existe uma variavel no codigo original chamada imas motor que perticipava
+ * do calculo da velocidade, mas quando ela foi aplicada nesse codigo, os valores ficaram fora da realidade
+ * e sem ela os resultados foram melhores. É preciso fazer um estudo mais a fundo sobre isso
+*/
+void calcularVelocidade() {
+    if (!conta) {
+        velocidade = 3.6 * 10e4 * circunferencia_do_motor * PI / deltaT ;
+        pulsos = false;
+        conta = true;
     } else {
-        // Marca o tempo quando a sequencia nao eh satisfeita
-        tempo_anterior_motor = micros();
+        velocidade = 0;
     }
 }
 
-void Velocidade_Motor :: motor_setup(){
-    
-    // Cria um serial com sua "velocidade de leitura"
+/**
+ * Esse método realiza a média móvel
+*/
+float mediaMovel() {
+    float soma = 0;
+    for (int i = 0; i < tamanhoMediaMovel; i++) {
+        soma += ultimosValores[i];
+    }
+    return soma / tamanhoMediaMovel;
+}
+
+/**
+ * Esse método pertence a média movel
+*/
+void adicionarValorAoBuffer(float valor) {
+    ultimosValores[indiceAtual] = valor;
+    indiceAtual = (indiceAtual + 1) % tamanhoMediaMovel;
+}
+
+void Velocidade_Motor::motor_setup() {
     Serial.begin(115200);
-
-    // Inicia o tempo atual como 0
-    tempo_atual = 0;
-
-    // Inicia a contagem dos intervalos de, por exemplo, 0 e 1 como 0 
-    contagem_motor = 0;
-
-    // Inicia o tempo anterior do motor como 0
-    tempo_anterior_motor = 0;
-
-    // Inicia a velocidade instantanea como 0
-    velocidade_instantanea_motor = 0;
-
-    // Le o estado logivco do pino do hall
-    pinMode(hall_motor, INPUT);
-
-    instance = this;
+    // Le o estado logico do pino do hall
+    pinMode(hall_motor, INPUT_PULLUP);
 
     // Funcao que fica vigiando a mudanca de estado do hall para a entrada na função do calculo do intervalo
-    attachInterrupt(digitalPinToInterrupt(hall_motor), hallInterruptHandler, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(hall_motor), contador, FALLING);
 }
 
-void Velocidade_Motor :: imprimir (){
-    Serial.print ("Velocidade:  ");
-    Serial.print (this->calculo_velocidade_instantanea_motor);
-    Serial.println (" km/h ");
-    Serial.print ("Estado logico: ");
-    Serial.println (digitalRead(hall_motor));
+void Velocidade_Motor::imprimir() {
+    calcularVelocidade();
+    // Adiciona o valor atual ao buffer
+    adicionarValorAoBuffer(velocidade);
+    // Calcula a média móvel
+    float media = mediaMovel(); 
+    // Imprime o valor da velocidade
+    if (isfinite(media)) {
+    Serial.print(media, 4);
+    Serial.println(" km/h ");
+  }
 }
-
